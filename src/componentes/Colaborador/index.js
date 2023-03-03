3200584fafcdcd6c1ff92ebf7cc05a2fd7ba4556
PedroFFigueiredo <pedrohenrifagundesfigueiredo@gmail.com>
2023-03-03 10:25:30 -0300

    feat: Criando função de favoritar

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,18 @@
 import { IoMdCloseCircle } from 'react-icons/io';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar
+
+    }
+
     return (<div className="colaborador">
         <IoMdCloseCircle
             size={25}
@@ -14,6 +25,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito
+                    ? <AiFillHeart {...propsFavorito} color='#ff0000' />
+                    : <AiOutlineHeart {...propsFavorito} />
+                }
+            </div>
         </div>
     </div>)
 }
